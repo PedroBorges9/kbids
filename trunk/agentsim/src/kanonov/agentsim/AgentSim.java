87e2c91273308dc36377c664fedbd1cb0abc0881
urikanonov <urikanonov@291024c2-2c9c-11de-a28d-194e1977add9>
2009-04-19 18:57:31 +0000

    Finished the agent simulator Removed the bin folders from SVN

--- trunk/agentsim/src/kanonov/agentsim/AgentSim.java
+++ trunk/agentsim/src/kanonov/agentsim/AgentSim.java
@@ -1,5 +1,8 @@
 package kanonov.agentsim;
 
+import java.util.*;
+
+import dt.fe.*;
 import dt.processor.Processor;
 import android.app.Activity;
 import android.content.ComponentName;
@@ -7,13 +10,21 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.widget.CompoundButton;
+import android.os.RemoteException;
+import android.os.SystemClock;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.*;
+
 
 public class AgentSim extends Activity implements ServiceConnection{
 	private static final String PROCESSOR_INTENT_ACTION = "dt.processor.kbta.action.PROCESSOR_SERVICE";
 	
+	
 	private ToggleButton _controlButton;
+	private Button _plus;
+	private Button _minus;
+	private TextView _interval;
 
 	private Processor _processor;
 	
@@ -24,6 +35,30 @@ public class AgentSim extends Activity implements ServiceConnection{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
+		_interval = (TextView)findViewById(R.id.interval);
+		_interval.setText("2");
+		
+
+		_plus = (Button)findViewById(R.id.plus);
+		_plus.setOnClickListener(new View.OnClickListener(){
+
+			@Override
+			public void onClick(View v){
+				_interval.setText(String.valueOf(getInterval() + 1));
+			}
+			
+		});
+		
+		_minus = (Button)findViewById(R.id.minus);
+		_minus.setOnClickListener(new View.OnClickListener(){
+
+			@Override
+			public void onClick(View v){
+				_interval.setText(String.valueOf(Math.max(1, getInterval() - 1)));
+			}
+			
+		});
+		
 		_controlButton = (ToggleButton)findViewById(R.id.control_button);
 		_controlButton
 				.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
@@ -34,12 +69,15 @@ public class AgentSim extends Activity implements ServiceConnection{
 							if (!bindService(new Intent(
 									PROCESSOR_INTENT_ACTION)
 									.addCategory("dt.agent.category.PROCESSOR"),
-								AgentSim.this, BIND_AUTO_CREATE)){
-								Toast.makeText(AgentSim.this, "Unable to connect to processor", Toast.LENGTH_SHORT);
-								_controlButton.setChecked(true);
+								AgentSim.this, BIND_AUTO_CREATE)){								
+								Toast.makeText(AgentSim.this, "Unable to connect to processor", Toast.LENGTH_SHORT).show();
+								_controlButton.setChecked(false);
 							}
 						}else{
 							unbindService(AgentSim.this);
+							_thread.interrupt();
+							_thread = null;
+							Toast.makeText(AgentSim.this, "Disconnected from processor", Toast.LENGTH_SHORT).show();
 						}
 					}
 				});
@@ -51,13 +89,41 @@ public class AgentSim extends Activity implements ServiceConnection{
 		if (service == null){
 			Toast.makeText(AgentSim.this, "Received null binder from processor", Toast.LENGTH_SHORT);
 		}else{
-			Toast.makeText(AgentSim.this, "Connected to processor", Toast.LENGTH_SHORT);
+			Toast.makeText(AgentSim.this, "Connected to processor", Toast.LENGTH_SHORT).show();
 			_processor = Processor.Stub.asInterface(service);
+			
+			_thread = new Thread(){
+				@Override
+				public void run(){
+					while (!isInterrupted()){
+						SystemClock.sleep(getInterval() * 1000);
+						
+						if (_processor != null){
+							List<MonitoredData> list = new ArrayList<MonitoredData>();
+							ParcelableDate d = new ParcelableDate();
+							
+							list.add(new MonitoredData("CPU_Usage", (int)(Math.random() * 100), d));
+							list.add(new MonitoredData("Garbage_Collections", (int)(Math.random() * 300), d));
+							try{
+								_processor.receiveMonitoredData(list);
+							}catch(RemoteException e){
+								e.printStackTrace();
+							}
+						}
+					}
+				}
+			};
+			_thread.start();
 		}
 	}
 
 	@Override
 	public void onServiceDisconnected(ComponentName component){
-		Toast.makeText(AgentSim.this, "Disconnected from processor", Toast.LENGTH_SHORT);
+		Toast.makeText(AgentSim.this, "Processor crashed", Toast.LENGTH_SHORT).show();
+		_processor = null;
+	}
+
+	private int getInterval(){
+		return Integer.parseInt(_interval.getText().toString());
 	}
 }
