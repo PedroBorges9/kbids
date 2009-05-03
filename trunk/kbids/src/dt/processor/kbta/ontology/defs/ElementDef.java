8dde4747e449331b99cfcaddd98eab3022d73f04
urikanonov <urikanonov@291024c2-2c9c-11de-a28d-194e1977add9>
2009-05-03 08:42:05 +0000

    added last iteration created in elementDef, fixed EventContainer

--- trunk/kbids/src/dt/processor/kbta/ontology/defs/ElementDef.java
+++ trunk/kbids/src/dt/processor/kbta/ontology/defs/ElementDef.java
@@ -9,7 +9,16 @@ package dt.processor.kbta.ontology.defs;
  */
 public class ElementDef {
 	protected final String _name;
+	private int _lastCreated;
 	
+	public boolean assertNotCreatedIn(int iteration){
+		return _lastCreated!=iteration;
+	}
+
+	public void setLastCreated(int iteration){
+		_lastCreated = iteration;
+	}
+
 	public ElementDef(String name){
 		_name = name;
 	}
