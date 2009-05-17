63767157f1cdbee458d600926c275fcf2076ce1b
ankrye@gmail.com <ankrye@gmail.com@291024c2-2c9c-11de-a28d-194e1977add9>
2009-05-17 18:13:29 +0000

    started on patterns.

---
+++ trunk/kbids/src/dt/processor/kbta/ontology/PatternElementSymbolic.java
@@ -0,0 +1,13 @@
+package dt.processor.kbta.ontology;
+
+import dt.processor.kbta.threats.DurationCondition;
+
+public class PatternElementSymbolic extends PatternElements {
+	public String _value;
+
+	public PatternElementSymbolic(int type, int ordinal,
+			DurationCondition duration, String value) {
+		super(type, ordinal, duration);
+		_value = value;
+	}
+}
